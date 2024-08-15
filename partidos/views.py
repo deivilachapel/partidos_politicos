b23566f674822cae9dc27b185df55309ad5fb333
Deivi Lachapel <deivilachapel@gmail.com>
2024-08-15 21:12:42 +0200

    up codigo json

--- partidos/views.py
+++ partidos/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import JsonResponse
+from .models import Partido
 
 def lista_partidos(request):
-    partidos = [
-        {"nombre": "Partido A", "ideologia": "Centro"},
-        {"nombre": "Partido B", "ideologia": "Izquierda"},
-        {"nombre": "Partido C", "ideologia": "Derecha"},
-    ]
-    return JsonResponse(partidos, safe=False)
+    partidos = Partido.objects.all()
+    partidos_list = list(partidos.values('nombre', 'ideologia'))
+    return JsonResponse(partidos_list, safe=False)
